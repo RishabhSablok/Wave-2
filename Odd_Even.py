13ea99df08bfb1bff2ef0fb168972fa9e9a0282d
RishabhSablok <Rishabh.sablok21@ycdsbk12.ca>
2020-04-17 17:51:36 -0400

    Done Comments

--- Odd_Even.py
+++ Odd_Even.py
@@ -1,4 +1,6 @@
+# Take number from user
 number = int(input("Input a number: "))
+# Check if the number is odd or even and print the result
 if number // 2 == 0:
     print("The number is even")
 else:
