7bf0f9eeb3095485e3eebf4510cc0394023dbeb3
RishabhSablok <Rishabh.sablok21@ycdsbk12.ca>
2020-04-17 17:55:38 -0400

    Done month_to_day

---
+++ Month_to_days.py
@@ -0,0 +1,28 @@
+# Take info from the user
+month = input("Input the month: ")
+# Check how many days are there
+
+if month == "january":
+    print("This month has 31 days")
+if month == "february":
+    print("This month has 28/29 days")
+if month == "march":
+    print("This month has 31 days")
+if month == "april":
+    print("This month has 30 days")
+if month == "may":
+    print("This month has 31 days")
+if month == "june":
+    print("This month has 30 days")
+if month == "july":
+    print("This month has 31 days")
+if month == "august":
+    print("This month has 31 days")
+if month == "september":
+    print("This month has 30 days")
+if month == "october":
+    print("This month has 31 days")
+if month == "november":
+    print("This month has 30 days")
+if month == "december":
+    print("This month has 31 days")
